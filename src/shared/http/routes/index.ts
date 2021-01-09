f2729413b850bd2fa1eecdb152477a8c33993262
yancaa <yan.cesar@sistemavitto.com.br>
2021-01-09 10:29:56 -0300

    feature: Criação de usuários e listagem

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
-import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
+import productsRouter from '@modules/users/routes/users.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
+routes.use('/users', usersRouter);
 routes.use('/products', productsRouter);
 
 export default routes;
